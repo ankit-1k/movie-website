9071e55b74b742c4aa2f26f1090365eb07121d35
Ankit Kumar Panda <105581787+ankit-1k@users.noreply.github.com>
2023-10-29 20:23:48 +0530

    Update App.jsx v.1.0
    remove lorem text

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <Home />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nihil temporibus voluptatum sequi harum deleniti sed fugit alias adipisci praesentium dolores veniam ad ipsam id earum voluptatem, neque pariatur quia facilis voluptate illo labore saepe. Sapiente delectus possimus tenetur corporis voluptatem quasi pariatur! Cupiditate error alias neque, quibusdam mollitia rerum? Accusamus beatae, tempore labore necessitatibus voluptatum sint. Porro quod numquam minima magni. Labore cumque reiciendis dolorem suscipit adipisci ipsum expedita ullam perferendis non voluptatum. Quas veritatis illum laborum! Explicabo saepe, quia laudantium assumenda autem, ea recusandae et ut a impedit ratione possimus doloremque id vero similique perferendis necessitatibus blanditiis suscipit!
     </>
   );
 }
